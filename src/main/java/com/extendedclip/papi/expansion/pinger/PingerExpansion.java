338020e5d000b5eb8661c3ebb8b092fcba11f3ce
流光溢彩Last <dreamvoid@dreamvoid.ml>
2022-09-18 14:07:36 +0800

    修复版

--- src/main/java/com/extendedclip/papi/expansion/pinger/PingerExpansion.java
+++ src/main/java/com/extendedclip/papi/expansion/pinger/PingerExpansion.java
@@ -1,5 +1,18 @@
 package com.extendedclip.papi.expansion.pinger;
 
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.net.SocketException;
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import me.clip.placeholderapi.expansion.Cacheable;
 import me.clip.placeholderapi.expansion.Configurable;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
@@ -8,69 +21,56 @@ import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
-import java.io.*;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketException;
-import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
+/* loaded from: Expansion-pinger.jar:com/extendedclip/expansion/pinger/PingerExpansion.class */
 public class PingerExpansion extends PlaceholderExpansion implements Cacheable, Taskable, Configurable {
     private BukkitTask pingTask = null;
-
     private String online = "&aOnline";
-
     private String offline = "&cOffline";
-
-    private final Map<String, Pinger> servers = new ConcurrentHashMap<>();
-
-    private final Map<String, InetSocketAddress> toPing = new ConcurrentHashMap<>();
-
+    private final Map<String, Pinger> servers = new ConcurrentHashMap();
+    private final Map<String, InetSocketAddress> toPing = new ConcurrentHashMap();
     private int interval = 60;
 
     public Map<String, Object> getDefaults() {
         Map<String, Object> defaults = new HashMap<>();
-        defaults.put("check_interval", Integer.valueOf(30));
+        defaults.put("check_interval", 30);
         defaults.put("online", "&aOnline");
         defaults.put("offline", "&cOffline");
         return defaults;
     }
 
+    /* JADX WARN: Type inference failed for: r1v9, types: [com.extendedclip.expansion.pinger.PingerExpansion$1] */
     public void start() {
         String on = getString("online", "&aOnline");
-        this.online = (on != null) ? on : "&aOnline";
+        this.online = on != null ? on : "&aOnline";
         String off = getString("offline", "&cOffline");
-        this.offline = (off != null) ? off : "&cOffline";
+        this.offline = off != null ? off : "&cOffline";
         int time = getInt("check_interval", 60);
-        if (time > 0)
+        if (time > 0) {
             this.interval = time;
-        this.pingTask = (new BukkitRunnable() {
+        }
+        this.pingTask = new BukkitRunnable() { // from class: com.extendedclip.expansion.pinger.PingerExpansion.1
             public void run() {
-                if (PingerExpansion.this.toPing.isEmpty())
-                    return;
-                for (Map.Entry<String, InetSocketAddress> address : PingerExpansion.this.toPing.entrySet()) {
-                    PingerExpansion.Pinger r;
-                    try {
-                        r = new PingerExpansion.Pinger(address.getValue().getHostName(), address.getValue().getPort());
-                        if (r.fetchData()) {
-                            PingerExpansion.this.servers.put(address.getKey(), r);
-                            continue;
+                if (!PingerExpansion.this.toPing.isEmpty()) {
+                    for (Map.Entry<String, InetSocketAddress> address : PingerExpansion.this.toPing.entrySet()) {
+                        try {
+                            Pinger r = new Pinger(address.getValue().getHostName(), address.getValue().getPort());
+                            if (r.fetchData()) {
+                                PingerExpansion.this.servers.put(address.getKey(), r);
+                            } else if (PingerExpansion.this.servers.containsKey(address.getKey())) {
+                                PingerExpansion.this.servers.remove(address.getKey());
+                            }
+                        } catch (Exception e) {
                         }
-                        if (PingerExpansion.this.servers.containsKey(address.getKey()))
-                            PingerExpansion.this.servers.remove(address.getKey());
-                    } catch (Exception exception) {
                     }
                 }
             }
-        }).runTaskTimerAsynchronously(getPlaceholderAPI(), 20L, 20L * this.interval);
+        }.runTaskTimerAsynchronously(getPlaceholderAPI(), 20L, 20 * this.interval);
     }
 
     public void stop() {
         try {
             this.pingTask.cancel();
-        } catch (Exception exception) {
+        } catch (Exception e) {
         }
         this.pingTask = null;
     }
@@ -102,62 +102,63 @@ public class PingerExpansion extends PlaceholderExpansion implements Cacheable, 
 
     public String onPlaceholderRequest(Player p, String identifier) {
         int place = identifier.indexOf("_");
-        if (place == -1)
+        if (place == -1) {
             return null;
+        }
         String type = identifier.substring(0, place);
         String address = identifier.substring(place + 1);
         Pinger r = null;
-        for (String a : this.servers.keySet()) {
-            if (a.equalsIgnoreCase(address)) {
-                r = this.servers.get(a);
+        Iterator<String> it = this.servers.keySet().iterator();
+        while (true) {
+            if (it.hasNext()) {
+                String a = it.next();
+                if (a.equalsIgnoreCase(address)) {
+                    r = this.servers.get(a);
+                    break;
+                }
+            } else {
                 break;
             }
         }
-        if (r == null)
-            if (!this.toPing.containsKey(address)) {
-                int port = 25565;
-                String add = address;
-                if (address.contains(":")) {
-                    add = address.substring(0, address.indexOf(":"));
-                    try {
-                        port = Integer.parseInt(address.substring(address.indexOf(":") + 1));
-                    } catch (Exception exception) {
-                    }
+        if (r == null && !this.toPing.containsKey(address)) {
+            int port = 25565;
+            String add = address;
+            if (address.contains(":")) {
+                add = address.substring(0, address.indexOf(":"));
+                try {
+                    port = Integer.parseInt(address.substring(address.indexOf(":") + 1));
+                } catch (Exception e) {
                 }
-                this.toPing.put(address, new InetSocketAddress(add, port));
             }
-        if (type.equalsIgnoreCase("motd"))
-            return (r != null) ? r.getMotd() : "";
-        if (type.equalsIgnoreCase("count") || type.equalsIgnoreCase("players"))
-            return (r != null) ? String.valueOf(r.getPlayersOnline()) : "0";
-        if (type.equalsIgnoreCase("max") || type.equalsIgnoreCase("maxplayers"))
-            return (r != null) ? String.valueOf(r.getMaxPlayers()) : "0";
-        if (type.equalsIgnoreCase("pingversion") || type.equalsIgnoreCase("pingv"))
-            return (r != null) ? String.valueOf(r.getPingVersion()) : "-1";
-        if (type.equalsIgnoreCase("gameversion") || type.equalsIgnoreCase("version"))
-            return (r != null && r.getGameVersion() != null) ? r.getGameVersion() : "";
-        if (type.equalsIgnoreCase("online") || type.equalsIgnoreCase("isonline"))
-            return (r != null) ? this.online : this.offline;
-        return null;
+            this.toPing.put(address, new InetSocketAddress(add, port));
+        }
+        if (type.equalsIgnoreCase("motd")) {
+            return r != null ? r.getMotd() : "";
+        } else if (type.equalsIgnoreCase("count") || type.equalsIgnoreCase("players")) {
+            return r != null ? String.valueOf(r.getPlayersOnline()) : "0";
+        } else if (type.equalsIgnoreCase("max") || type.equalsIgnoreCase("maxplayers")) {
+            return r != null ? String.valueOf(r.getMaxPlayers()) : "0";
+        } else if (type.equalsIgnoreCase("pingversion") || type.equalsIgnoreCase("pingv")) {
+            return r != null ? String.valueOf(r.getPingVersion()) : "-1";
+        } else if (type.equalsIgnoreCase("gameversion") || type.equalsIgnoreCase("version")) {
+            return (r == null || r.getGameVersion() == null) ? "离线" : String.valueOf(r.getGameVersion());
+        } else if (!type.equalsIgnoreCase("online") && !type.equalsIgnoreCase("isonline")) {
+            return null;
+        } else {
+            return r != null ? this.online : this.offline;
+        }
     }
 
+    /* loaded from: Expansion-pinger.jar:com/extendedclip/expansion/pinger/PingerExpansion$Pinger.class */
     public final class Pinger {
-        private String address = "localhost";
-
-        private int port = 25565;
-
-        private int timeout = 2000;
-
-        private int pingVersion = -1;
-
-        private int protocolVersion = -1;
-
         private String gameVersion;
-
         private String motd;
-
+        private String address = "localhost";
+        private int port = 25565;
+        private int timeout = 2000;
+        private int pingVersion = -1;
+        private int protocolVersion = -1;
         private int playersOnline = -1;
-
         private int maxPlayers = -1;
 
         public Pinger(String address, int port) {
@@ -241,84 +242,76 @@ public class PingerExpansion extends PlaceholderExpansion implements Cacheable, 
             try {
                 Socket socket = new Socket();
                 socket.setSoTimeout(this.timeout);
-                socket.connect(
-                        new InetSocketAddress(getAddress(), getPort()),
-                        getTimeout());
+                socket.connect(new InetSocketAddress(getAddress(), getPort()), getTimeout());
                 OutputStream outputStream = socket.getOutputStream();
                 DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
                 InputStream inputStream = socket.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream,
-                        Charset.forName("UTF-16BE"));
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-16BE"));
                 dataOutputStream.write(new byte[]{-2, 1});
                 int packetId = inputStream.read();
                 if (packetId == -1) {
                     try {
                         socket.close();
-                    } catch (IOException iOException) {
+                    } catch (IOException e) {
                     }
-                    socket = null;
                     return false;
-                }
-                if (packetId != 255) {
+                } else if (packetId != 255) {
                     try {
                         socket.close();
-                    } catch (IOException iOException) {
+                    } catch (IOException e2) {
                     }
-                    socket = null;
                     return false;
-                }
-                int length = inputStreamReader.read();
-                if (length == -1) {
-                    try {
-                        socket.close();
-                    } catch (IOException iOException) {
-                    }
-                    socket = null;
-                    return false;
-                }
-                if (length == 0) {
-                    try {
-                        socket.close();
-                    } catch (IOException iOException) {
-                    }
-                    socket = null;
-                    return false;
-                }
-                char[] chars = new char[length];
-                if (inputStreamReader.read(chars, 0, length) != length) {
-                    try {
-                        socket.close();
-                    } catch (IOException iOException) {
-                    }
-                    socket = null;
-                    return false;
-                }
-                String string = new String(chars);
-                if (string.startsWith("&")) {
-                    String[] data = string.split("\000");
-                    setPingVersion(Integer.parseInt(data[0].substring(1)));
-                    setProtocolVersion(Integer.parseInt(data[1]));
-                    setGameVersion(data[2]);
-                    setMotd(data[3]);
-                    setPlayersOnline(Integer.parseInt(data[4]));
-                    setMaxPlayers(Integer.parseInt(data[5]));
                 } else {
-                    String[] data = string.split("&");
-                    setMotd(data[0]);
-                    setPlayersOnline(Integer.parseInt(data[1]));
-                    setMaxPlayers(Integer.parseInt(data[2]));
+                    int length = inputStreamReader.read();
+                    if (length == -1) {
+                        try {
+                            socket.close();
+                        } catch (IOException e3) {
+                        }
+                        return false;
+                    } else if (length == 0) {
+                        try {
+                            socket.close();
+                        } catch (IOException e4) {
+                        }
+                        return false;
+                    } else {
+                        char[] chars = new char[length];
+                        if (inputStreamReader.read(chars, 0, length) != length) {
+                            try {
+                                socket.close();
+                            } catch (IOException e5) {
+                            }
+                            return false;
+                        }
+                        String string = new String(chars);
+                        if (string.startsWith("§")) {
+                            String[] data = string.split("\000");
+                            setPingVersion(Integer.parseInt(data[0].substring(1)));
+                            setProtocolVersion(Integer.parseInt(data[1]));
+                            setGameVersion(data[2]);
+                            setMotd(data[3]);
+                            setPlayersOnline(Integer.parseInt(data[4]));
+                            setMaxPlayers(Integer.parseInt(data[5]));
+                        } else {
+                            String[] data2 = string.split("§");
+                            setMotd(data2[0]);
+                            setPlayersOnline(Integer.parseInt(data2[1]));
+                            setMaxPlayers(Integer.parseInt(data2[2]));
+                        }
+                        dataOutputStream.close();
+                        outputStream.close();
+                        inputStreamReader.close();
+                        inputStream.close();
+                        socket.close();
+                        return true;
+                    }
                 }
-                dataOutputStream.close();
-                outputStream.close();
-                inputStreamReader.close();
-                inputStream.close();
-                socket.close();
-            } catch (SocketException exception) {
+            } catch (SocketException e6) {
                 return false;
-            } catch (IOException exception) {
+            } catch (IOException e7) {
                 return false;
             }
-            return true;
         }
     }
 }
